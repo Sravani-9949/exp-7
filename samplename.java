e3f04ae3e71f62b3e35f852cc0d2853a9824b3a1
Ananya5h7 <annareddynandini51@gmail.com>
2025-10-07 14:56:18 +0900

    samplename.java

---
+++ samplename.java
@@ -0,0 +1,8 @@
+samplename.java
+public class samplename
+{
+    public static void main(String args[])
+    {
+        String name="Sravani";
+        System.out.println("Hello,My name is + name");
+    }
